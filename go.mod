c31ca3c49a202c7c4e348164d0d7b6add9a14d98
Ilmari Vacklin <ilmari.vacklin@iki.fi>
2019-08-21 10:39:44 +0300

    initial implementation

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/wolverian/pipes
 
 go 1.12
+
+require (
+	github.com/aws/aws-sdk-go v1.23.4
+	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/codepipeline"
+)
 
 func main() {
-	fmt.Println("pipes")
+	name := os.Args[1]
+	if name == "" {
+		fmt.Fprintln(os.Stderr, "usage: pipes <pipeline name>")
+	}
+
+	sess := getSession()
+
+	cp := codepipeline.New(sess)
+
+	out, err := cp.GetPipelineState(&codepipeline.GetPipelineStateInput{
+		Name: aws.String(name),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, st := range out.StageStates {
+		fmt.Printf("%s: %s\n", *st.StageName, *st.LatestExecution.Status)
+	}
+}
+
+func getSession() *session.Session {
+	return session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+		Config:            *aws.NewConfig().WithCredentialsChainVerboseErrors(true),
+	}))
 }
