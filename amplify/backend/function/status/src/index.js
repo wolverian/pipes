beb7d437c3967caf90dc14428db3dd52f63c8dac
Ilmari Vacklin <ilmari.vacklin@iki.fi>
2019-08-22 16:07:37 +0300

    Simplify function

--- amplify/backend/function/status/src/index.js
+++ amplify/backend/function/status/src/index.js
@@ -1,9 +1,12 @@
-const awsServerlessExpress = require('aws-serverless-express');
-const app = require('./app');
+const awsServerlessExpress = require("aws-serverless-express");
+const app = require("./app");
 
 const server = awsServerlessExpress.createServer(app);
 
 exports.handler = (event, context) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
-  awsServerlessExpress.proxy(server, event, context);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Nice!" })
+  };
 };
